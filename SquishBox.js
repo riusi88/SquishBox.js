837f955058290ba2178bd2ea2bc55076f3a941dd
riusi88 <riusi88@gmail.com>
2014-06-23 16:17:27 -0400

    Optimized JS and added a Minified version

--- SquishBox.js
+++ SquishBox.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){
-        $(".box1").css({position: 'relative', float: 'left'});
-        $(".box2").css({position: 'relative', float: 'right'});
-        $(".box3").css({position: 'relative', float: 'left'});
-        $(".box4").css({position: 'relative', float: 'right'});
-	$(".box1").animate({height:'50%',width:'50%'},0, 'swing');
-	$(".box2").animate({height:'50%',width:'50%'},0, 'swing');
-	$(".box3").animate({height:'50%',width:'50%'},0, 'swing');
-	$(".box4").animate({height:'50%',width:'50%'},0, 'swing');
+//SquishBox June. 23. 2014.
+//By: Ryan Iusi
+//Copyright: Ryan Iusi
 
+
+$(document).ready(function(){
 	
+//Styling the boxes
+        $(".box1").css({position: 'relative', float: 'left',height:'50%',width:'50%'});
+        $(".box2").css({position: 'relative', float: 'right',height:'50%',width:'50%'});
+        $(".box3").css({position: 'relative', float: 'left',height:'50%',width:'50%'});
+        $(".box4").css({position: 'relative', float: 'right',height:'50%',width:'50%'});
+		
+//Hover Events.
 $(".box1").hover(function() {
   $(this).stop().animate({left:'0%', width: '75%', height: '75%',},'swing');
   $(".box2").stop().animate({height:'25%',width:'25%'},'swing');
